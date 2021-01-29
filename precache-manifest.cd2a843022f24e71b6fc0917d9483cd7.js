ce5170eb9d15f5f177887da5800eeef30bec2ba1
Saad Bakht <rotk13@gmail.com>
2021-01-29 15:33:11 -0500

    deploy

---
+++ precache-manifest.cd2a843022f24e71b6fc0917d9483cd7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "05b21a49082e955750ce",
+    "url": "/halal-competitions-frontend/css/app.ddd1c39f.css"
+  },
+  {
+    "revision": "05b21a49082e955750ce",
+    "url": "/halal-competitions-frontend/js/app.a505e8f7.js"
+  },
+  {
+    "revision": "6a98d0b2686f7cc1bb61",
+    "url": "/halal-competitions-frontend/js/chunk-vendors.a7ac9fe8.js"
+  },
+  {
+    "revision": "98526dca9b679036c0fe0a6041adae84",
+    "url": "/halal-competitions-frontend/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/halal-competitions-frontend/robots.txt"
+  }
+];
